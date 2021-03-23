2fd28a33f6666483b26e8d18702c4a71ce945523
Fangyi Zhou <me@fangyi.io>
2021-03-23 17:15:35 +0000

    Register callbacks for functions in Solib

--- src/pedrolib.ml
+++ src/pedrolib.ml
@@ -1,1 +1,9 @@
-let () = Callback.register "main" Pedrolib.Lib.main
+open Pedrolib
+
+let () =
+  Callback.register "main" Lib.main ;
+  (* Solib exported functions *)
+  Callback.register "load_from_file" Solib.load_from_file ;
+  Callback.register "save_to_file" Solib.save_to_file ;
+  Callback.register "get_enabled_transitions" Solib.get_enabled_transitions ;
+  Callback.register "do_transition" Solib.do_transition
