c94b72fede8b16a1807fa65a17d254c6faa4d31e
Francisco Ferreira <fferreira@gmail.com>
2021-03-24 19:29:21 +0000

    fix to silent transitions

--- src/lib/global.ml
+++ src/lib/global.ml
@@ -12,21 +12,26 @@ type state =
   ; net : net (* the net we are building *)
   }
 
+(* token from role *)
+let tkr (r : N.RoleName.t) : name = N.RoleName.show r
+
 module Translation = struct
   include Monad.State (struct
     type s = state
   end)
 
+  (* generates a fresh name *)
   let gen_sym () : name t =
     let* st = get in
     let* _ = set {st with gen_sym_st = st.gen_sym_st + 1}  in
     "_" ^ (string_of_int st.gen_sym_st) |> return
 
+  (* looks up a role in gamma *)
   let lookup_gamma (r : N.RoleName.t) : name option t =
     let* st = get in
     List.assoc_opt r st.gamma |> return
 
-
+  (* asserts that pl is a place in the current net *)
   let assert_place pl =
     let* st = get in
     let n = st.net in
@@ -34,7 +39,8 @@ module Translation = struct
     | None -> fail (pl ^ " is not a place")
     | Some _ -> return ()
 
-  let create_silent_tr src dst =
+  (* create a silent transition between two places *)
+  let create_silent_tr src dst tkn =
     let* _ = assert_place src in
     let* _ = assert_place dst in
     let* st = get in
@@ -42,7 +48,7 @@ module Translation = struct
     let* nm = gen_sym () in
     let net = {n with
                 transitions = (nm, Silent)::n.transitions
-              ; arcs = (src, nm, PlaceToTransition, [])::(nm, dst, TransitionToPlace, []):: n.arcs
+              ; arcs = (src, nm, PlaceToTransition, [tkn])::(nm, dst, TransitionToPlace, [tkn]):: n.arcs
               }
     in
     set {st with net}
@@ -53,12 +59,11 @@ module Monadic = struct
 
   (* base operations *)
 
-
   let bring (r : N.RoleName.t) (dst : name) : unit t =
     let* from = lookup_gamma r in
     match from with
     | None -> return ()
-    | Some src -> create_silent_tr src dst
+    | Some src -> create_silent_tr src dst (tkr r)
 
 
 
