3c049ccd85b7f7e42f23783d16a2153621fbfe86
Francisco Ferreira <fferreira@gmail.com>
2021-03-29 18:22:34 +0100

    new user interface

--- src/lib/lib.ml
+++ src/lib/lib.ml
@@ -35,76 +35,56 @@ let validate_exprs_to_net_to_exprs_to_net exprs =
     | _ -> failwith "Violation: this cannot happen" )
   | _ -> failwith "Violation: this cannot happen"
 
-let main_scr fn =
-  try
-    let scr = Nuscrlib.Lib.parse fn (Stdlib.open_in fn) in
-    let protocol_names = List.map fst @@ Nuscrlib.Lib.enumerate scr in
-    let gtypes =
-      List.map
-        (fun proto -> Nuscrlib.Lib.get_global_type scr ~protocol:proto)
-        protocol_names
-    in
-    (* let proto = Nuscrlib.Names.ProtocolName.of_string "Test" in
-     * let gtype = Nuscrlib.Lib.get_global_type scr ~protocol:proto in *)
-    let get_net gtype =
-      match Global.net_of_global_type gtype with
-      | Ok net -> net
-      | Error err -> failwith err
-    in
-    List.iter
-      (fun (nm, gt) ->
-        "--- Protocol: " ^ nm ^ " ---" |> print_endline ;
-        Syntax.expr_list_of_net (get_net gt)
-        |> Pretty.pp_expr_list |> print_endline )
-      (List.combine
-         (List.map Nuscrlib.Names.ProtocolName.user protocol_names)
-         gtypes )
-  with Nuscrlib.Err.UserError ue ->
-    Nuscrlib.Err.show_user_error ue |> print_endline
+type fmt
+  = Nuscr
+  | Pedro
+  | Dot
+  | Sexp
+  | Info
 
-let main_pdr fn =
-  print_endline @@ "//Reading: " ^ fn ;
-  let exprs = parse fn (Stdlib.open_in fn) in
-  match Syntax.validate_net exprs with
-  | Ok net ->
-      print_endline "----Graphviz----" ;
-      Pn.generate_ppn net |> Pn.generate_dot |> print_endline ;
-      print_endline "----SExp----" ;
-      Syntax.sexp_of_net net |> Sexplib.Sexp.to_string_hum |> print_endline ;
-      print_endline "----Pedro----" ;
-      Pretty.pp_expr_list exprs |> print_endline ;
-      print_endline "----Pedro from net----" ;
-      let net' = net |> Syntax.expr_list_of_net |> Pretty.pp_expr_list in
-      print_endline net' ;
-      print_endline "----Information----" ;
-      "Enabled transitions: "
-      ^ (Opsem.enabled_transitions net |> String.concat " ")
-      |> print_endline ;
-      "Enabled transitions (with silent): "
-      ^ (Opsem.enabled_transitions_with_silent net |> String.concat " ")
-      |> print_endline ;
-      "Enabled transitions (with silent, and pretty print back): "
-      ^ ( Opsem.enabled_transitions_with_silent
-          @@ validate_exprs_to_net_to_exprs_to_net exprs
-        |> String.concat " " )
-      |> print_endline ;
-      print_endline "----After first transition----" ;
-      if List.length (Opsem.enabled_transitions net) > 0 then (
-        let net =
-          Opsem.enabled_transitions net
-          |> List.hd |> Opsem.do_transition net
-          |> Option.value ~default:Syntax.empty_net
-        in
-        net |> Pn.generate_ppn |> Pn.generate_dot |> print_endline ;
-        print_endline "----Information after the first transition ----" ;
-        "Enabled transitions: "
-        ^ (net |> Opsem.enabled_transitions |> String.concat " ")
-        |> print_endline ;
-        "Enabled transitions: "
-        ^ (net |> Opsem.enabled_transitions_with_silent |> String.concat " ")
-        |> print_endline )
-      else "No first transition." |> print_endline
-  | Error err -> "//Alles kaputt!: " ^ err |> print_endline
+let main (fmt_in : fmt) (fmt_out : fmt) fn =
+  let inp : (string * Syntax.net) list =
+    match fmt_in with
+    | Pedro ->
+         let exprs = parse fn (Stdlib.open_in fn) in
+         begin match Syntax.validate_net exprs with
+         | Ok net -> [fn, net]
+         | Error err -> failwith err
+         end
+    | Nuscr ->
+       begin try
+           let scr = Nuscrlib.Lib.parse fn (Stdlib.open_in fn) in
+           let protocol_names = List.map fst @@ Nuscrlib.Lib.enumerate scr in
+           let gtypes =
+             let scr_to_net proto =
+               match Nuscrlib.Lib.get_global_type scr ~protocol:proto |> Global.net_of_global_type with
+               | Ok net -> net
+               | Error err -> failwith err
+             in
+             List.map
+               (fun proto -> Global.N.ProtocolName.user proto, scr_to_net proto)
+               protocol_names
+           in
+           gtypes
+         with Nuscrlib.Err.UserError ue -> Nuscrlib.Err.show_user_error ue |> failwith
+       end
+    | _ -> failwith "input format not supported"
+  in
 
-let main fn =
-  if Filename.check_suffix fn ".pdr" then main_pdr fn else main_scr fn
+  let out (nm, net) =
+    "**** " ^ nm ^ "****\n" ^
+      match fmt_out with
+      | Nuscr -> failwith "output format not supported"
+      | Pedro -> net |> Syntax.expr_list_of_net |> Pretty.pp_expr_list
+      | Dot -> Pn.generate_ppn net |> Pn.generate_dot
+      | Sexp -> Syntax.sexp_of_net net |> Sexplib.Sexp.to_string
+      | Info ->
+         "----Information----\n"
+         ^ "Enabled transitions: "
+         ^ (Opsem.enabled_transitions net |> String.concat " ") ^ "\n"
+         ^ "Enabled transitions (with silent): "
+         ^ (Opsem.enabled_transitions_with_silent net |> String.concat " ")
+  in
+
+  let str : string = String.concat "\n" @@ List.map out inp in
+  print_endline str
--- src/pedro.ml
+++ src/pedro.ml
@@ -1,7 +1,18 @@
+let fmt_of_string = function
+    "nuscr" -> Pedrolib.Lib.Nuscr
+  | "pedro" -> Pedrolib.Lib.Pedro
+  | "dot" -> Pedrolib.Lib.Dot
+  | "sexp" -> Pedrolib.Lib.Sexp
+  | "info" -> Pedrolib.Lib.Info
+  | err -> failwith @@ "Unknown option: " ^ err
+
 let () =
-  print_endline "//Vote for Pedro!" ;
-  print_endline @@ "//Current working directory: " ^ Sys.getcwd () ;
-  let fn =
-    if Array.length Sys.argv = 2 then Sys.argv.(1) else "examples/proto.pdr"
-  in
-  Pedrolib.Lib.main fn
+  print_endline "(*) Vote for Pedro!" ;
+  print_endline @@ "(*) Current working directory: " ^ Sys.getcwd () ;
+
+  if Array.length Sys.argv = 4 then
+    let inp = Sys.argv.(1) |> fmt_of_string in
+    let out = Sys.argv.(2) |> fmt_of_string in
+    let fn = Sys.argv.(3) in
+    Pedrolib.Lib.main inp out fn
+  else "Invalid arguments" |> print_endline
