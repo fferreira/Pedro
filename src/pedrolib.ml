d36a6a696e06d7b35f539f611c36b1416d396c13
Fangyi Zhou <me@fangyi.io>
2022-01-05 14:58:11 +0000

    Forgot to actually export the function

--- src/pedrolib.ml
+++ src/pedrolib.ml
@@ -8,4 +8,5 @@ let () =
   Callback.register "get_enabled_transitions" Solib.get_enabled_transitions ;
   Callback.register "do_transition" Solib.do_transition ;
   Callback.register "has_finished" Solib.has_finished ;
-  Callback.register "commit_hash" Solib.commit_hash
+  Callback.register "commit_hash" Solib.commit_hash ;
+  Callback.register "get_all_roles" Solib.get_all_roles
